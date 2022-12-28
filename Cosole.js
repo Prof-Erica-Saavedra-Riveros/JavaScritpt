a4dc611f45d2f4f165a6328a80c92897108479a4
Prof-Erica-Saavedra-Riveros <prof.ericasaavedra@gmail.com>
2022-12-28 12:55:07 -0600

    Mis datos

--- Cosole.js
+++ Cosole.js
@@ -1,1 +1,14 @@
+var json_parse= '{ "nombre": "Erica", "edad": "46", "nacionalidad": "Mexicana"  }'
+var obj_parse=JSON.parse(json_parse)
+console.log(obj_parse)
+var json= { 
+    nombre: "Erica", 
+    edad: 46, 
+    nacionalidad: "Mexicana" }
+console.log(json)
 
+var json = '{"nombre":"Adrian","edad":23,"nacionalidad":"Mexicana","hobbies":["ajedrez","video juegos","comics"]}';
+var obj = JSON.parse(json);
+console.log(obj.hobbies[0]);
+obj.hobbies[0] = "peliculas";
+console.log(obj.hobbies[0]);
